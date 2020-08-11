c3c7251273b2d3b21b120614e226533dfb402fbf
Pedroasdf@1 <pedrodutra1r@gmail.com>
2020-08-11 12:57:11 -0300

    NLW2

---
+++ public/scripts/addField.js
@@ -0,0 +1,16 @@
+document.querySelector('#add-time')
+    .addEventListener('click', cloneField)
+
+function cloneField() {
+    const newFieldsContainer = document.querySelector('.schedule-item').cloneNode(true)
+    
+    const inputs = newFieldsContainer.querySelectorAll('input')
+
+    inputs.forEach(function(input) {
+
+        input.value = ''
+
+    })
+
+    document.querySelector('#schedule-items').appendChild(newFieldsContainer)
+}
